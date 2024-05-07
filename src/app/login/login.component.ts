69b187a5341b0a1001b15ccd11f1ca9de6b92015
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-05-07 15:36:02 +0100

    jwt

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,11 +21,25 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  checkLogin() {
+  /*checkLogin() {
     if (this.loginservice.authenticate(this.username, this.password)) {
       this.router.navigate([''])
     } else
       this.invalidLogin = true
+  }*/
+  checkLogin() {
+
+    this.loginservice.authenticate(this.username, this.password).subscribe(
+      (data:any) => {
+        this.router.navigate([''])
+        this.invalidLogin = false
+        console.log(data)
+      },
+      (error:any) => {
+        this.invalidLogin = true
+        console.log(error)
+      }
+    );
   }
 
 }
--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
 
@@ -11,12 +11,14 @@ export class LogoutComponent implements OnInit {
 
   constructor(
     private authenticationService: AuthenticationService,
-    private router: Router) {
+    private router: Router,
+    private cdr: ChangeDetectorRef) {
 
   }
 
   ngOnInit() {
     this.authenticationService.logOut();
+    this.cdr.detectChanges()
     this.router.navigate(['login']);
   }
 
